57fe369bedb273cfb3a5efb768ade28391146054
dugles220 <orskmineru@mail.ru>
2026-07-05 13:54:56 +0300

    docs(pixels): add pixels.h documentation

--- include/pixels.h
+++ include/pixels.h
@@ -1,3 +1,7 @@
+/**
+ * @file pixels.h
+ * @brief Модуль работы с пикселями и полотном пикселей
+ */
 #ifndef PIXELS_H
 #define PIXELS_H
 
@@ -10,7 +14,25 @@ RGB **allocate_pixels(size_t height, size_t width);
 void write_pixels(Pixels *image, int left_up_x, int left_up_y, RGB **pixels, size_t height, size_t width);
 RGB **rotate_pixels(const RGB **pixels, size_t height, size_t width);
 void free_pixels_arr(RGB **pixels);
+/**
+ * @brief Перекрашивает пиксель в заданный цвет
+ * 
+ * @param pixel Пиксель, который нужно перекрасить
+ * @param color Цвет, в который необходимо перекрасить пиксель
+ */
 void repaint_pixel(RGB *pixel, const RGB *color);
+/**
+ * @brief Рисует точку заданной толщины и цвета на полотне пикселей
+ *
+ * Если часть точки выходит за пределы изображения, то эта часть не
+ * отрисовывается. 
+ *  
+ * @param pixels Полотно пикселей
+ * @param x Координата X точки
+ * @param y Координата Y точки
+ * @param thickness Толщина точки
+ * @param color Цвет точки
+ */
 void draw_pixel(Pixels *pixels, int x, int y, int thickness, const RGB *color);
 
 #endif
